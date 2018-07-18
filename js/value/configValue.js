0ef4e38426857405d85030bdbc4a51759abf17ce
Renan Ramos <renanrramossi@gmail.com>
2018-07-18 17:07:05 -0300

    Realizada correção dos recursos fornecidos pela API de 'http:\' para 'https:\', pois estava gerando problemas em navegadores 'Mixed Content'.

--- js/value/configValue.js
+++ js/value/configValue.js
@@ -1,13 +1,13 @@
 angular.module('sww').value("config", {
-    baseUrlPessoas: 'http://swapi.co/api/people/?page=',
+    baseUrlPessoas: 'https://swapi.co/api/people/?page=',
     baseSearchUrlPessoa: 'https://swapi.co/api/people/?search=',
     
     baseUrlFilmes: 'https://swapi.co/api/films/',
     baseSearchUrlFilmes: 'https://swapi.co/api/films/?search=',
 
-    baseUrlPlanetas: 'http://swapi.co/api/planets/?page=',
+    baseUrlPlanetas: 'https://swapi.co/api/planets/?page=',
     baseSearchUrlPlanetas: 'https://swapi.co/api/planets/?search=',
     
-    baseUrlVeiculos: 'http://swapi.co/api/vehicles/?page=',
-    baseSearchUrlVeiculos: 'http://swapi.co/api/vehicles/?search='
+    baseUrlVeiculos: 'https://swapi.co/api/vehicles/?page=',
+    baseSearchUrlVeiculos: 'https://swapi.co/api/vehicles/?search='
 });
